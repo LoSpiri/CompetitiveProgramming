effbe30476edf538e1bb8686cfd307c4322308ef
LoSpiri <spiri16@protonmail.com>
2023-09-24 19:00:48 +0200

    MissingNumber

---
+++ MissingNumber/MissingNumber.rs
@@ -0,0 +1,10 @@
+fn missing_number(nums: Vec<i32>) -> i32 {
+    let len = nums.len();
+    let sum_usize = (len * (len + 1)) / 2;
+    let sum_i32: i32 = sum_usize as i32;
+    let mut actual_sum = 0;
+    for num in nums {
+        actual_sum += num;
+    }
+    return sum_i32 - actual_sum;
+}
