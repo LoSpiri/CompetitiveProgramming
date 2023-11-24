afdf1e19eafcddf072e587af3860d1fe4df100b7
LoSpiri <spiri16@protonmail.com>
2023-11-24 01:06:54 +0100

    Completed both parts and included testing

--- Hands-On/hands_on_2/src/lib.rs
+++ Hands-On/hands_on_2/src/lib.rs
@@ -9,7 +9,7 @@ impl SegmentTree {
         let n = input.len();
         let next_pow_of_two = (2 * n - 1).next_power_of_two();
         let segment_tree = vec![i32::MIN; 2 * next_pow_of_two - 1];
-        let lazy = vec![0; 2 * next_pow_of_two - 1];
+        let lazy = vec![i32::MAX; 2 * next_pow_of_two - 1];
 
         let mut tree = SegmentTree { tree: segment_tree, lazy, length: n };
 
@@ -20,11 +20,11 @@ impl SegmentTree {
 
 
     pub fn update_segment_tree_range_lazy(&mut self, start_range: usize, end_range: usize, delta: i32) {
-        self.update_segment_tree_range_lazy_helper(start_range, end_range, delta, 0, self.length - 1, 0);
+        self.update_segment_tree_range_lazy_helper(start_range - 1, end_range - 1, delta, 0, self.length - 1, 0);
     }
 
     pub fn range_maximum_query_lazy(&mut self, qlow: usize, qhigh: usize) -> i32 {
-        self.range_maximum_query_lazy_helper(qlow, qhigh, 0, self.length - 1, 0)
+        self.range_maximum_query_lazy_helper(qlow - 1, qhigh - 1, 0, self.length - 1, 0)
     }
 
     fn construct_max_segment_tree(&mut self, input: &[i32], low: usize, high: usize, pos: usize) {
@@ -41,7 +41,7 @@ impl SegmentTree {
 
     fn update_segment_tree_range_lazy_helper(&mut self, start_range: usize, end_range: usize, delta: i32, low: usize, high: usize, pos: usize) {
         // Update the lazy value for the current node if needed
-        if self.lazy[pos] != 0 {
+        if self.lazy[pos] != i32::MAX {
             self.tree[pos] = self.tree[pos].min(self.lazy[pos]);
 
             // If not a leaf node, propagate the lazy value to children
@@ -51,7 +51,7 @@ impl SegmentTree {
             }
 
             // Reset the lazy value for the current node
-            self.lazy[pos] = 0;
+            self.lazy[pos] = i32::MAX;
         }
 
         // No overlap
@@ -85,23 +85,17 @@ impl SegmentTree {
 
     fn range_maximum_query_lazy_helper(&mut self, qlow: usize, qhigh: usize, low: usize, high: usize, pos: usize) -> i32 {
         // Update the lazy value for the current node if needed
-        if self.lazy[pos] != 0 {
-            if let Some(result) = self.tree[pos].checked_add(self.lazy[pos]) {
-                self.tree[pos] = result;
-            } else {
-                // Handle overflow (you can choose an appropriate strategy)
-                // Here, we set the value to i32::MAX, you may want to adjust it based on your requirements.
-                self.tree[pos] = i32::MIN;
-            }
+        if self.lazy[pos] != i32::MAX {
+            self.tree[pos] = self.tree[pos].min(self.lazy[pos]);
 
             // If not a leaf node, propagate the lazy value to children
             if low != high {
-                self.lazy[2 * pos + 1] += self.lazy[pos];
-                self.lazy[2 * pos + 2] += self.lazy[pos];
+                self.lazy[2 * pos + 1] = self.lazy[2 * pos + 1].min(self.lazy[pos]);
+                self.lazy[2 * pos + 2] = self.lazy[2 * pos + 1].min(self.lazy[pos]);
             }
 
             // Reset the lazy value for the current node
-            self.lazy[pos] = 0;
+            self.lazy[pos] = i32::MAX;
         }
 
         // No overlap
@@ -119,8 +113,160 @@ impl SegmentTree {
         let left_child = self.range_maximum_query_lazy_helper(qlow, qhigh, low, mid, 2 * pos + 1);
         let right_child = self.range_maximum_query_lazy_helper(qlow, qhigh, mid + 1, high, 2 * pos + 2);
 
-        // Return the minimum value from both children
+        // Return the maximum value from both children
         return left_child.max(right_child);
+    }
+
+}
+
+
+// ###########################################################################################################
+// #                                            PARTE 2                                                      #
+// ###########################################################################################################
+
+pub struct SegmentTree2 {
+    pub tree: Vec<i32>,
+    pub lazy: Vec<i32>,
+    length: usize
+}
+
+impl SegmentTree2 {
+    pub fn new(input: &Vec<(usize, usize)>) -> SegmentTree2 {
+        let n = input.len();
+        let next_pow_of_two = (2 * n - 1).next_power_of_two();
+        let segment_tree = vec![0; 2 * next_pow_of_two - 1];
+        let lazy = vec![0; 2 * next_pow_of_two - 1];
+
+        let mut tree = SegmentTree2 { tree: segment_tree, lazy, length: n };
+
+        for &(first, second) in input.iter() {
+            tree.update_segment_tree_range_lazy_2(first, second);
+        }
+
+        tree
+    }
+    
+    pub fn update_segment_tree_range_lazy_2(&mut self, start_range: usize, end_range: usize) {
+        self.update_segment_tree_range_lazy_helper(start_range, end_range, 0, self.length - 1, 0);
+    }
+
+    pub fn range_maximum_query_lazy_array(&mut self, input: &Vec<(usize, usize, usize)>) -> Vec<i32> {
+        let mut result: Vec<i32> = Vec::new();
+        for &(first, second, third) in input.iter() {
+            let out = self.range_maximum_query_lazy(first, second, third as i32);
+            if out > 0 {
+                result.push(1);
+            } else {
+                result.push(0);
+            }
+        }
+        return result;
+    }
+
+    pub fn range_maximum_query_lazy(&mut self, qlow: usize, qhigh: usize, key: i32) -> i32 {
+        self.range_maximum_query_lazy_helper(qlow, qhigh, 0, self.length - 1, 0, key)
+    }
+
+    fn update_segment_tree_range_lazy_helper(&mut self, start_range: usize, end_range: usize, low: usize, high: usize, pos: usize) {
+        // Update the lazy value for the current node if needed
+        if self.lazy[pos] != 0 {
+            if let Some(result) = self.tree[pos].checked_add(self.lazy[pos]) {
+                self.tree[pos] = result;
+            } else {
+                self.tree[pos] = i32::MAX;
+            }
+
+            // If not a leaf node, propagate the lazy value to children
+            if low != high {
+                self.lazy[2 * pos + 1] += self.lazy[pos];
+                self.lazy[2 * pos + 2] += self.lazy[pos];
+            }
+
+            // Reset the lazy value for the current node
+            self.lazy[pos] = 0;
+        }
+
+        // No overlap
+        if start_range > high || end_range < low {
+            // println!("NO OVERLAP: START - {} - END - {} - LOW - {} - HIGH - {} - POS - {}", start_range, end_range, low, high, pos);
+            return;
+        }
+
+        // Total overlap
+        if start_range <= low && end_range >= high {
+            // println!("TOTAL OVERLAP: START - {} - END - {} - LOW - {} - HIGH - {} - POS - {}", start_range, end_range, low, high, pos);
+            self.tree[pos] += 1;
+
+            // If not a leaf node, propagate the lazy value to children
+            if low != high {
+                self.lazy[2 * pos + 1] += 1;
+                self.lazy[2 * pos + 2] += 1;
+            }
+            return;
+        }
+
+        // println!("PARTIAL OVERLAP: START - {} - END - {} - LOW - {} - HIGH - {} - POS - {}", start_range, end_range, low, high, pos);
+        // Partial overlap, update both children
+        let mid = (low + high) / 2;
+        self.update_segment_tree_range_lazy_helper(start_range, end_range, low, mid, 2 * pos + 1);
+        self.update_segment_tree_range_lazy_helper(start_range, end_range, mid + 1, high, 2 * pos + 2);
+
+        // Update the parent node based on the children
+        self.tree[pos] = self.tree[2 * pos + 1].max(self.tree[2 * pos + 2]);
+        // println!("Updated parent at pos: {} with value: {}", pos, self.tree[pos])
+    }
+
+    fn range_maximum_query_lazy_helper(&mut self, qlow: usize, qhigh: usize, low: usize, high: usize, pos: usize, key: i32) -> i32 {
+        // Update the lazy value for the current node if needed
+        if self.lazy[pos] != 0 {
+            if let Some(result) = self.tree[pos].checked_add(self.lazy[pos]) {
+                self.tree[pos] = result;
+            } else {
+                // Handle overflow (you can choose an appropriate strategy)
+                // Here, we set the value to i32::MAX, you may want to adjust it based on your requirements.
+                self.tree[pos] = i32::MAX;
+            }
+
+            // If not a leaf node, propagate the lazy value to children
+            if low != high {
+                self.lazy[2 * pos + 1] += self.lazy[pos];
+                self.lazy[2 * pos + 2] += self.lazy[pos];
+            }
+
+            // Reset the lazy value for the current node
+            self.lazy[pos] = 0;
+        }
+        
+        // In case the key is greater than the max number of intersection of segments
+        if self.tree[pos] < key {
+            return 0;
+        }
+
+        // No overlap
+        if qlow > high || qhigh < low {
+            // println!("NO OVERLAP: START - {} - END - {} - LOW - {} - HIGH - {} - POS - {}", qlow, qhigh, low, high, pos);
+            return 0;
+        }
+
+        // Total overlap
+        if qlow <= low && qhigh >= high {
+            // println!("TOTAL OVERLAP: START - {} - END - {} - LOW - {} - HIGH - {} - POS - {}", qlow, qhigh, low, high, pos);
+            if self.tree[pos] == key {
+                return 1;
+            }
+            if low == high {
+                return 0;
+            }
+        }
+
+        // println!("PARTIAL OVERLAP: START - {} - END - {} - LOW - {} - HIGH - {} - POS - {}", qlow, qhigh, low, high, pos);
+        // Partial overlap, query both children
+        let mid = (low + high) / 2;
+        let left_child = self.range_maximum_query_lazy_helper(qlow, qhigh, low, mid, 2 * pos + 1, key);
+        let right_child = self.range_maximum_query_lazy_helper(qlow, qhigh, mid + 1, high, 2 * pos + 2, key);
+
+        // Return the minimum value from both children
+        return left_child + right_child;
     }
 
 }
--- Hands-On/hands_on_2/src/main.rs
+++ Hands-On/hands_on_2/src/main.rs
@@ -1,76 +1,170 @@
 use hands_on_2::SegmentTree;
+use hands_on_2::SegmentTree2;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io;
+use std::io::BufRead;
+use std::num::ParseIntError;
 
-use std::path::{PathBuf, Path};
-use std::str::SplitWhitespace;
+fn read_output(file_path: &str) -> io::Result<Vec<i32>> {
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
 
-fn main() {
-    
-    // -----------------------------
-    for i in 0..=1 {
-        let file_path: PathBuf = format!("test1/input{}.txt", i).into();
-        if let Ok(file) = File::open(file_path) {
-            let reader = BufReader::new(file);
-            let mut array_length: i32;
-            let mut total_queries: i32;
-            let mut counter = 0;
-            let mut st: SegmentTree = SegmentTree::new(&[0]);
-            let mut result_max_queries: Vec<i32> = Vec::new();
-            for line in reader.lines() {
-                if let Ok(line_content) = line {
-                    // println!("{}", line_content);
+    let numbers: io::Result<Vec<i32>> = reader.lines().map(|line| {
+        line?.parse::<i32>().map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+    }).collect();
 
-                    let values: Result<Vec<i32>, _> = line_content
+    numbers
+}
+
+fn read_input_1(file_path: &str) -> io::Result<(Vec<i32>, Vec<(usize, usize, usize, Option<Result<usize, ParseIntError>>)>)> {
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+    let mut lines = reader.lines();
+
+    let first_line = lines.next().ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Empty file"))??;
+    let mut n_m_iter = first_line
+    .split_whitespace()
+    .map(
+        |s| s.parse::<usize>()
+    );
+    let _n = match n_m_iter.next() {
+        Some(Ok(value)) => value,
+        _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid N")),
+    };
+    let m = match n_m_iter.next() {
+        Some(Ok(value)) => value,
+        _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid M")),
+    };
+
+    let second_line = lines.next().ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Empty file"))??;
+    let array: Vec<i32> = second_line
                     .split_whitespace()
-                    .map(|s| s.parse::<i32>())
+                    .filter_map(|s| s.parse::<i32>().ok())
                     .collect();
 
-                    match values {
-                        Ok(parsed_values) => {
-                            if counter == 0 {
-                                array_length = parsed_values[0];
-                                total_queries = parsed_values[1];
-                            } else if counter == 1 {
-                                st = SegmentTree::new(&parsed_values);
-                                println!("{:?}", st.tree);
-                            } else {
-                                if parsed_values[0] == 0 {
-                                    st.update_segment_tree_range_lazy(parsed_values[1] as usize - 1, parsed_values[2] as usize - 1, parsed_values[3]);
-                                } else if parsed_values [0] == 1 {
-                                    result_max_queries.push(st.range_maximum_query_lazy(parsed_values[1] as usize - 1, parsed_values[2] as usize - 1));
-                                } else {
-                                    eprintln!("Query is neither update nor max");
-                                }
-                            }
-                        }
-                        Err(err) => {
-                            // Gestione dell'errore di parsing
-                            eprintln!("Error parsing values: {}", err);
-                        }
-                    }
-                    counter += 1;
-                } else {
-                    eprintln!("Error reading line from file");
+    let mut queries = Vec::with_capacity(m as usize);
+
+    for line in lines.take(m as usize) {
+        let line = line?;
+        let mut values_iter = line
+        .split_whitespace()
+        .map(
+            |s| s.parse::<usize>()
+        );
+
+        if let Some(Ok(first)) = values_iter.next() {
+            if let Some(Ok(second)) = values_iter.next() {
+                if let Some(Ok(third)) = values_iter.next() {
+                    let query = if let Some(fourth) = values_iter.next() {
+                        (first, second, third, Some(fourth))
+                    } else {
+                        (first, second, third, None)
+                    };
+                    queries.push(query);
                 }
             }
-            println!("{:?}", result_max_queries);
-        } else {
-            eprintln!("Error opening file");
         }
     }
-    // -----------------------------
+
+    Ok((array, queries))
+}
+
+fn read_input_2(file_path: &str) -> io::Result<(Vec<(usize, usize)>, Vec<(usize, usize, usize)>)> {
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+    let mut lines = reader.lines();
+
+    let first_line = lines.next().ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Empty file"))??;
+    let mut n_m_iter = first_line
+    .split_whitespace()
+    .map(
+        |s| s.parse::<usize>()
+    );
+    let n = match n_m_iter.next() {
+        Some(Ok(value)) => value,
+        _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid N")),
+    };
+    let m = match n_m_iter.next() {
+        Some(Ok(value)) => value,
+        _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid M")),
+    };
+
+    let mut segments: Vec<(usize, usize)>= Vec::with_capacity(n as usize);
+    let mut queries: Vec<(usize, usize, usize)> = Vec::with_capacity(m as usize);
+
+    for line in lines.take(n as usize + m as usize) {
+        let line = line?;
+        let mut values_iter = line
+        .split_whitespace()
+        .map(
+            |s| s.parse::<usize>()
+        );
+
+        if let Some(Ok(first)) = values_iter.next() {
+            if let Some(Ok(second)) = values_iter.next() {
+                if values_iter.clone().count() == 0 {
+                    segments.push((first, second));
+                } else if let Some(Ok(third)) = values_iter.next() {
+                    queries.push((first, second, third));
+                }
+            }
+        }
+    }
+    Ok((segments, queries))
+}
+
+
+fn main() {
+    // -------------------------------------------------------------------------------------------------
+    // TESTING PARTE 1
+    // NOTE (per me):
+
+    for i in 0..=10 {
+        if let Ok((array, queries)) = read_input_1(&format!("test1/input{}.txt", i)) {
+            let mut st: SegmentTree = SegmentTree::new(&array);
+            let mut result_queries_1: Vec<i32> = Vec::new();
+            for (query_type, start, end, key) in queries {
+                if query_type == 1 {
+                    result_queries_1.push(st.range_maximum_query_lazy(start, end));
+                } else {
+                    let parsed_key = key.and_then(|result| result.ok());
+                    match parsed_key {
+                        Some(parsed_key) => {
+                            st.update_segment_tree_range_lazy(start, end, parsed_key as i32);
+                        }
+                        None => {
+                            eprintln!("Should be an update query but there's no key.");
+                        }
+                    }
+                }
+            }
+            if let Ok(output_1) = read_output(&format!("test1/output{}.txt", i)) {
+                assert_eq!(output_1, result_queries_1);
+                println!("{:?}", result_queries_1);
+            }
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------------
+    // TESTING PARTE 2 
+    // NOTE (per me):
+    // La query si ferma prima anche se la chiave da trovare é maggiore del valore attuale 
+
+    for i in 0..=7 {
+        if let Ok((segments, queries)) = read_input_2(&format!("test2/input{}.txt", i)) {
+            let mut st_2: SegmentTree2 = SegmentTree2::new(&segments);
+            let result_queries_2 = st_2.range_maximum_query_lazy_array(&queries);
     
-    // let mut st_lazy = SegmentTree::new(&[2, 6, 7, 0, 2, 1]);
-    // println!("{:?}", st_lazy.tree);
-    // println!("{:?}", st_lazy.lazy);
-    // assert_eq!(7, st_lazy.range_maximum_query_lazy(2, 5));
-    // st_lazy.update_segment_tree_range_lazy(2, 4, 4);
-    // // st_lazy.update_segment_tree_range_lazy(0, 3, 1);
-    // // st_lazy.update_segment_tree_range_lazy(0, 0, 2);
-    // assert_eq!(4, st_lazy.range_maximum_query_lazy(2, 5));
-    // println!("{:?}", st_lazy.tree);
-    // println!("{:?}", st_lazy.lazy);
+            if let Ok(output_2) = read_output(&format!("test2/output{}.txt", i)) {
+                assert_eq!(output_2, result_queries_2);
+                println!("{:?}", result_queries_2);
+            }
+        } else {
+            eprintln!("Error reading file.");
+        }
+    } 
+    // -------------------------------------------------------------------------------------------------
+
     println!("IO SONO ERMELLINO")
 }
